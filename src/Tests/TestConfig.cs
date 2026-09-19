be95177fa9427b5086228ee2631ff793a7a5e184
Simon Cropp <simon.cropp@gmail.com>
2026-09-19 20:00:29 +1000

    Update TestConfig.cs

--- src/Tests/TestConfig.cs
+++ src/Tests/TestConfig.cs
@@ -1,1 +1,5 @@
-﻿[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
+﻿using Xunit.v3;
+using Xunit.Sdk;
+
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
+[assembly: Parallelization(Mode = ParallelMode.None)]
